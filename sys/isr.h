ec14951a94fcdefa45c7a6cffc2cfb1347d79685
Chris Smith <chrsmith@google.com>
2015-02-16 20:07:07 -0800

    Cleaned up regs, not sure if correct.

--- sys/isr.h
+++ sys/isr.h
@@ -1,14 +1,38 @@
 #ifndef SYS_ISR_H_
 #define SYS_ISR_H_
 
-/* This defines what the stack looks like after an ISR was running */
-struct regs
-{
-    unsigned int gs, fs, es, ds;      /* pushed the segs last */
-    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
-    unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
-    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */ 
-};
+#include "lib/types.h"
+
+// Registers when the ISR was triggered. Used for (hopefully) diagnosing bugs.
+typedef struct {
+  // Pushed the segments last.
+  uint32_t gs;
+  uint32_t fs;
+  uint32_t es;
+  uint32_t ds;
+
+  // Pushed by "pusha".
+  uint32_t edi;
+  uint32_t esi;
+  uint32_t ebp;
+  uint32_t esp;
+
+  uint32_t ebx;
+  uint32_t edx;
+  uint32_t ecx;
+  uint32_t eax;
+
+  // "push byte #" and ecodes does this.
+  uint32_t int_no;
+  uint32_t err_code;
+
+  // Pushed by the processor automcially.
+  uint32_t eip;
+  uint32_t cs;
+  uint32_t eflags;
+  uint32_t useresp;
+  uint32_t ss;
+} regs;
 
 void isr_install();
 
