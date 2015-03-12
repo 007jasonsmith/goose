37a6b7ceb482c7d7ef13fbe5ded2d9e282e80e02
Chris Smith <chrsmith@google.com>
2015-03-11 20:52:12 -0700

    New printf complete

--- src/klib/new_printf_test.cpp
+++ src/klib/new_printf_test.cpp
@@ -158,6 +158,13 @@ TEST(ArgAccumulator, UInt64) {
 class IOutputFn {
  public:
   virtual void Print(char c) = 0;
+
+  void Print(const char* msg) {
+    while (*msg) {
+      Print(*msg);
+      msg++;
+    }
+  }
 };
 
 // Wrap a function for pinting characters. Used to provide
@@ -415,11 +422,73 @@ TEST(TypePrinter, HexError) {
   EXPECT_STREQ(p.Get(), "[ERROR: invalid type for hex]");
 }
 
+template<typename... Args>
+void printf(const char* format, IOutputFn* out, Args... func_args) {
+  ArgAccumulator args = ArgAccumulator::Parse(func_args...);
+  TypePrinter tp(out);
+
+  int i = 0;
+  int arg_index = 0;
+  while (format[i] != 0) {
+    char c = format[i];
+    if (c != '%') {
+      out->Print(c);
+    } else {
+      if (arg_index >= args.Count()) {
+	out->Print("[Error: args underspecified]");
+	return;
+      }
+
+      char next = format[i + 1];
+      if (next == 0) {
+	out->Print("[ERROR: missing format specifier]");
+	return;
+      }
+
+      tp.Print(args.Get(arg_index));
+
+      arg_index++;
+      i++;  // Skip over next
+    }
+    i++;
+  }
+
+  while (arg_index < args.Count()) {
+    out->Print(" Extra: ");
+    tp.Print(args.Get(arg_index));
+    arg_index++;
+  }
+}
+
 // TODO(chris): Tests for new printf and friends.
 // - Test underflow
 // - Test overflow
 // - Test no args
 // - Test many args
+
+TEST(Printf, Basic) {
+  TestPrinter p;
+  printf("hello, world!", &p);
+  EXPECT_STREQ(p.Get(), "hello, world!");
+}
+
+TEST(Printf, Basic2) {
+  TestPrinter p;
+  printf("[%n] [%c] [%s]", &p, -11, '@', "hello, world");
+  EXPECT_STREQ(p.Get(), "[-11] [@] [hello, world]");
+}
+
+TEST(Printf, Extra) {
+  TestPrinter p;
+  printf("%d", &p, 1, 2, 3);
+  EXPECT_STREQ(p.Get(), "1 Extra: 2 Extra: 3");
+}
+
+TEST(Printf, ErrorUnderspecified) {
+  TestPrinter p;
+  printf("%d %d %d", &p, 1, 2);
+  EXPECT_STREQ(p.Get(), "1 2 [Error: args underspecified]");
+}
 
 int main (int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
